ae1bd451dba5c60f8d68f1bdfb89dccd90d16fe1
Todor Kazakov <todd.kazakov@gmail.com>
2018-11-09 12:51:39 +0200

    Update :retry to 0.11

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule OverPowered.Mixfile do
      {:httpotion, "~> 3.0.0"},
      {:ecto, "~> 2.2"},
      {:cachex, "~> 3.0"},
-     {:retry, "~> 0.8.0"},
+     {:retry, "~> 0.11"},
      {:logger_json, "~> 2.0.1"},
      {:jason, "~> 1.1"}
     ]
